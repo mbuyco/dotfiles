e4201b5d874ad81e318428d25b8a9141d3ac6d63
Mike Buyco <michael.buyco@messagemedia.com>
2021-07-03 09:04:12 +0800

    add rbenv to path

--- omf/init.fish
+++ omf/init.fish
@@ -3,5 +3,10 @@ bass source $HOME/dotfiles/aliases/main
 bass source $HOME/dotfiles/aliases/docker
 bass source $HOME/dotfiles/aliases/git
 
-# export local bin
+# export path
 export PATH="$PATH:$HOME/.local/bin"
+
+# export rbenv
+if [ -d "$HOME/.rbenv/bin" ];
+    export PATH="$PATH:$HOME/.rbenv/bin"
+end
