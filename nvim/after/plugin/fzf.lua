5aaad3808ce936d7a2f67b4a1f85276d39e5f318
Mike Buyco <michael.buyco@messagemedia.com>
2022-01-19 08:42:29 +0800

    update nvim config

--- nvim/after/plugin/fzf.lua
+++ nvim/after/plugin/fzf.lua
@@ -1,3 +1,7 @@
 vim.api.nvim_set_keymap('n', '<c-P>',
     "<cmd>lua require('fzf-lua').files()<CR>",
     { noremap = true, silent = true })
+
+vim.api.nvim_set_keymap('n', '<c-F>',
+    "<cmd>lua require('fzf-lua').live_grep_native()<CR>",
+    { noremap = true, silent = true })
